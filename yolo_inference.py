690587d32900f7f3eda6d73a69c73c17b629e9ab
Bryan Zhu <bzhu29@gmail.com>
2025-12-27 17:08:50 -0500

    updated to yolov11 and detect only ball and people

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,9 +1,12 @@
 from ultralytics import YOLO
 
-model = YOLO('yolov8m')
+#model = YOLO('yolov8m')
+model = YOLO('yolo11l.pt')
+model.classes = [0]  # only detect person class (class 0 in COCO dataset)
 
-results = model.predict('test_video.mp4', save=True)
-print(results[0])
+
+results = model.predict('test_276-290_1080p.mp4',classes=[0,32], save=True)
+print(type(results))#[0])
 print('==========================')
 for box in results[0].boxes:
     print(box)
