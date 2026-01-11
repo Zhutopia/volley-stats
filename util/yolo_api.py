ad8a261669da0809a2aeb4e15f2786f657cae499
unknown <bzhu29@gmail.com>
2026-01-11 10:01:07 -0500

    updated yolo_api.py to define the run_inference function

--- util/yolo_api.py
+++ util/yolo_api.py
@@ -1,14 +1,9 @@
 from ultralytics import YOLO
 
-model = YOLO('yolov5x6u')
-#model = YOLO("models/player_detector.pt")
-model.classes = [0]  # only detect person class (class 0 in COCO dataset)
+def run_inference(model_path='models/myFirstTraining.pt', video_path='videos/test_video.mp4')
+    model = YOLO(model_path)
+    results = model.predict(video_path, save=True)
 
-#result = model.track('test_276-290_1080p.mp4', conf=0.65, classes=[0], persist=True, save=True, show=True, stream=False)
-results = model.predict('input_videos/test_276-290_1080p.mp4', conf=0.65, save=True)
-
-#results = model.predict('test_276-290_1080p.mp4',classes=[0],conf=0.65 save=True)
-print(type(results))#[0])
-print('==========================')
-for box in results[0].boxes:
-    print(box)
+    print('==========================')
+    for box in results[0].boxes:
+        print(box)
